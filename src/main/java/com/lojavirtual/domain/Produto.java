9caebfc691ca15637a834c4258dc005230848872
Aninha2006 <aninhapimentadoida1234@gmail.com>
2021-04-23 13:28:35 -0300

    associando produtos com as categorias

--- src/main/java/com/lojavirtual/domain/Produto.java
+++ src/main/java/com/lojavirtual/domain/Produto.java
@@ -1,15 +1,22 @@
 package com.lojavirtual.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
 @Entity
-@Table(name="produto")
+@Table(name="PRODUTO")
 public class Produto implements Serializable {
 
 	private static final long serialVersionUID = 1L;
@@ -22,9 +29,17 @@ public class Produto implements Serializable {
 	
 	private double preco;
 	
+	@ManyToMany
+	@JoinTable(name = "PRODUTO_CATEGORIA",
+			joinColumns = @JoinColumn (name = "produto_id"),
+			inverseJoinColumns = @JoinColumn (name = "categoria_id"))
+	@JsonBackReference
+	private List<Categoria> categorias = new ArrayList<>();
+	
 	public Produto () {}
 
 
+	
 	public Produto(Integer id, String nome, double preco) {
 		super();
 		this.id = id;
@@ -57,7 +72,16 @@ public class Produto implements Serializable {
 		this.preco = preco;
 	}
 	
-	
+	public List<Categoria> getCategorias() {
+		return categorias;
+	}
+
+	public void setCategorias(List<Categoria> categorias) {
+		this.categorias = categorias;
+	}
+
+
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
