14bf25131bf7afc0b4617d4eef3842ae6570f7e0
Aninha2006 <aninhapimentadoida1234@gmail.com>
2021-04-24 00:16:02 -0300

    Criando camada de serviço do pagamento

---
+++ src/main/java/com/lojavirtual/domain/PagamentoComCartao.java
@@ -0,0 +1,30 @@
+package com.lojavirtual.domain;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "PAGAMENTO COM CARTAO")
+public class PagamentoComCartao implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	private Integer numeroDeParcelas; 
+ 
+	public  PagamentoComCartao () {}
+
+	public PagamentoComCartao(Integer numeroDeParcelas) {
+		super();
+		this.numeroDeParcelas = numeroDeParcelas;
+	}
+
+	public Integer getNumeroDeParcelas() {
+		return numeroDeParcelas;
+	}
+
+	public void setNumeroDeParcelas(Integer numeroDeParcelas) {
+		this.numeroDeParcelas = numeroDeParcelas;
+	}
+}
